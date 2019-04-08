c509fbc71cb57d5fd9577952ed48b6b9df5ed47f
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 05:06:44 +0000

    Snapshot of branch master taken on 2019-04-08T01:06:43.509-04:00 from "https://github.com/UnofficialJuliaMirror/staticfloat-LLVMBuilder"

---
+++ check-patches.jl
@@ -0,0 +1,35 @@
+##
+# Check that patches between BinaryBuilder and Julia are consistent.
+# julia check_patches.jl .../path/to/julia/patchdir
+##
+
+@assert length(ARGS) == 1
+
+DIR = joinpath(@__DIR__, "patches", "llvm_patches")
+patches = readdir(DIR)
+
+files = Dict(map(x -> x => joinpath(DIR, x), patches))
+orig_files = Dict(map(x -> x => joinpath(ARGS[1], x[6:end]), patches))
+
+for patch in patches
+    orig = orig_files[patch]
+    file = files[patch]
+
+    if !isfile(orig)
+        warn("$patch is not present in $(ARGS[1])")
+        continue
+    end
+    try
+        run(`diff $orig $file`)
+    catch
+      warn("$patch differs")
+    end
+end
+
+patches = map(p->string(p[6:end]), patches)
+
+for patch in readdir(ARGS[1])
+  if startswith(patch, "llvm") && !(patch in patches)
+     warn("$patch not present for LLVMBuilder")
+  end
+end
